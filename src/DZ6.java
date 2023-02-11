1028dd2b18533f36709110ef4b964283be6a8d8a
Roman <79613377185R@gmail.com>
2023-02-11 15:28:40 +0300

    DZ6 Itog Java

--- src/DZ6.java
+++ src/DZ6.java
@@ -5,14 +5,19 @@ public class DZ6 {
          notebooks.add(new Notebook(4, 500, 22200, "Windows", "Lenova 3350"));
          notebooks.add(new Notebook(8, 1000, 32800, "Linux","Xpeng 2046"));
          notebooks.add(new Notebook(16, 2000, 45800, "Mac","Mac"));
-         notebooks.add(new Notebook(16, 1000, 38000, "Mac","mac"));
+         notebooks.add(new Notebook(16, 1000, 38000, "Mac","Mac"));
          notebooks.add(new Notebook(4, 500, 22200, "Windows","MSI 1298"));
 
-         Map<String, Integer> filterCriteria = new HashMap<>();
+         filtr (notebooks);
+     }
 
+     static void filtr (List<Notebook> notebooks){
+         Map<String, Integer> filterCriteria = new HashMap<>();
          Scanner scanner = new Scanner(System.in);
-         System.out.println("Enter filter criteria (1 - RAM, 2 - Storage, 3 - Operating System, 4 - Price): ");
+         System.out.println("Enter filter criteria (1 - RAM, 2 - Storage, 3 - Operating System, 4 - Price, 5 - Model): ");
          int criteria = scanner.nextInt();
+         int sistem=0;
+         int mod=0;
 
          switch (criteria) {
              case 1:
@@ -24,23 +29,23 @@ public class DZ6 {
                  filterCriteria.put("Storage", scanner.nextInt());
                  break;
              case 3:
-                 System.out.println("Enter operating system: ");
-                 filterCriteria.put("Operating System", 0);
+                 System.out.println("Enter operating system:\n 1 - Windows\n 2- Linux\n 3- Mac\n ");
+                 sistem=scanner.nextInt();
+                 filterCriteria.put("Operating System", sistem);
                  break;
              case 4:
                  System.out.println("Enter maximum Price: ");
                  filterCriteria.put("Price", scanner.nextInt());
                  break;
              case 5:
-                 System.out.println("Enter model: ");
-                 filterCriteria.put("model", 0);
+                 System.out.println("Modeli v nalichie, enter Model;\n 1-Lenova 3350\n 2-Xpeng 2046\n 3- MSI 1298\n 4- Mac");
+                 mod=scanner.nextInt();
+                 filterCriteria.put("Model", 0);
                  break;
              default:
                  System.out.println("Invalid filter criteria.");
                  break;
          }
-
-         List<Notebook> filteredNotebooks = new ArrayList<>();
          System.out.println("Filtered notebooks: ");
          boolean flaf = false;
          for (Notebook notebook : notebooks) {
@@ -55,9 +60,12 @@ public class DZ6 {
                  flaf=true;
              }
              if (filterCriteria.containsKey("Operating System")) {
-                 scanner.nextLine();
-                 String os = scanner.nextLine();
-                 if (!notebook.operatingSystem.equalsIgnoreCase(os)) {
+                 String os = "";
+                 if(sistem==1){os="Windows";}
+                 if(sistem==2){os="Linux";}
+                 if(sistem==3){os="Mac";}
+
+                 if (notebook.operatingSystem.equals(os)) {
                      System.out.println(notebook.model+" Storage:"+notebook.storage+" RAM:"+
                              notebook.RAM+ " Price:"+notebook.price);
                      flaf=true;
@@ -67,6 +75,19 @@ public class DZ6 {
                  System.out.println(notebook.model+" Storage:"+notebook.storage+" RAM:"+
                          notebook.RAM+" Operation System:"+notebook.operatingSystem+ " Price:"+notebook.price);
                  flaf=true;
+             }
+             if (filterCriteria.containsKey("Model")) {
+                 String model="";
+                 if(mod==1){model="Lenova 3350";}
+                 if(mod==2){model="Xpeng 204";}
+                 if(mod==3){model="MSI 1298";}
+                 if(mod==4){model="Mac";}
+
+                 if (notebook.model.equals(model)) {
+                     System.out.println(notebook.model+" Storage:"+notebook.storage+" RAM:"+
+                             notebook.RAM+ " Price:"+notebook.price);
+                     flaf=true;
+                 }
              }
          }
          if (flaf==false) {
